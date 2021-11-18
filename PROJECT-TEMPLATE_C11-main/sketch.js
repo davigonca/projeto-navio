cff38a00d629e3e4b3b1cdfdd991949fad1687cf
davigonca <93744285+davigonca@users.noreply.github.com>
2021-11-18 16:37:46 -0300

    Add files via upload

---
+++ PROJECT-TEMPLATE_C11-main/sketch.js
@@ -0,0 +1,18 @@
+var marImg,navioImg
+function preload(){
+marImg = loadImage("sea.png");
+navioImg = loadAnimation (ship1.png,ship2.png);
+spriteName.addImage(marImg);
+}
+
+function setup(){
+  createCanvas(400,400);
+  
+}
+
+function draw() {
+  background("blue");
+    drawSprites();
+    
+ 
+}
